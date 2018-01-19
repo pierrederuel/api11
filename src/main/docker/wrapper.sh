6684b5cfeae9a693bea0ac402fe01f4700fd79f9
Pierre Deruel <pierre.deruel@gmail.com>
2018-01-19 18:35:01 +0100

    Update wrapper.sh

--- src/main/docker/wrapper.sh
+++ src/main/docker/wrapper.sh
@@ -2,7 +2,7 @@
 
 ##while ! exec 6<>/dev/tcp/${DATABASE_HOST}/${DATABASE_PORT}; do
     echo "Trying to connect to MySQL at ${DATABASE_PORT}..."
-    sleep 60
+    sleep 30
 ##done
 
 java -Djava.security.egd=file:/dev/./urandom -Dspring.profiles.active=container -jar /app.jar
