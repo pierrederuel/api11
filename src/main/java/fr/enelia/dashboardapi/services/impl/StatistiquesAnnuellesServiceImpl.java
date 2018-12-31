375a976e679a8adaa30388460aaf1603e0cce3c5
Pierre Deruel <pierre.deruel@gmail.com>
2018-12-31 10:22:42 +0100

    Update StatistiquesAnnuellesServiceImpl.java

--- src/main/java/fr/enelia/dashboardapi/services/impl/StatistiquesAnnuellesServiceImpl.java
+++ src/main/java/fr/enelia/dashboardapi/services/impl/StatistiquesAnnuellesServiceImpl.java
@@ -13,6 +13,8 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.Iterator;
+
 @Service("statistiquesAnnuellesService")
 public class StatistiquesAnnuellesServiceImpl implements StatistiquesAnnuellesService {
 
